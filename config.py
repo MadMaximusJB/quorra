ae5ed9f23b22d8f86df25518b63eefad9f657ecf
MadMaximusJB <62156106+MadMaximusJB@users.noreply.github.com>
2024-06-25 19:09:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY'
 GOOGLE_APPLICATION_CREDENTIALS = 'C:\\path\\to\\your\\credentials.json'  # Use double backslashes
 DEFAULT_VOICE_NAME = 'en-US-Journey-O'  # Replace with your desired voice
 DEFAULT_AUDIO_ENCODING = 'LINEAR16'  # Use LINEAR16 for streaming audio
+GOOGLE_CSE_API_KEY = 'YOUR_CSE_API_KEY'
+GOOGLE_CSE_ID = 'YOUR_CSE_ID'
